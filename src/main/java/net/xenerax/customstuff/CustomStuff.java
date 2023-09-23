53dbff5b29bef41831fd9769d7b2d67a7f74553c
xenerax <saeland.florian@gmail.com>
2023-09-23 10:11:11 +0200

    Initial commit

---
+++ src/main/java/net/xenerax/customstuff/CustomStuff.java
@@ -0,0 +1,17 @@
+package net.xenerax.customstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomStuff implements ModInitializer {
+	public static final String Mod_ID = "xenerax-custom-stuff-mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(Mod_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/xenerax/customstuff/CustomStuffClient.java
@@ -0,0 +1,10 @@
+package net.xenerax.customstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
